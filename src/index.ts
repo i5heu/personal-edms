c84bf4322a0a4def240ac1812b357fe4e486e4c4
i5heu <i5heu@i5h.eu>
2021-02-13 12:57:31 +0100

    add rate limiting

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,35 @@
 console.info("Server starting....");
 import express from "express";
+import { reqCheck, resetIpCache, loginLeft } from "./limitChecks";
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database(":memory:");
 const app = express();
 
-app.set('view engine', 'pug');
+resetIpCache();
 
-app.get("/", (res, req) => {
-  req.render("login");
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  if (!reqCheck(req, res)) return;
+  res.render("login", { loginLeft: loginLeft(req) });
 });
 
-app.listen(80, () =>
-  console.info("Server serving.")
-);
+app.post("/rest/login", (req, res) => {
+  if (!req.body.mail || !req.body.pwd) {
+    res.status(500).send("You have to insert e-mail and password!");
+    return;
+  }
+
+  if (!reqCheck(req, res, true)) return;
+
+  res.redirect("/");
+});
+
+app.listen(80, () => console.info("Server serving."));
---
+++ src/limitChecks.ts
@@ -0,0 +1,39 @@
+import express from "express";
+
+let ipCache: number[] = [];
+let ipLoginCache: number[] = [];
+export function resetIpCache() {
+  ipCache = [];
+  setTimeout(resetIpCache, 10000);
+}
+
+export function loginLeft(req: express.Request): number {
+  if (!ipLoginCache[req.ip]) return 5;
+  return 5 - ipLoginCache[req.ip];
+}
+
+export function reqCheck(
+  req: express.Request,
+  res: express.Response,
+  login = false
+): boolean {
+  if (login) {
+    if (!ipLoginCache[req.ip]) ipLoginCache[req.ip] = 0;
+    ipLoginCache[req.ip]++;
+
+    if (ipLoginCache[req.ip] > 5) {
+      res.status(429).send("429 Too Many Requests");
+      return false;
+    }
+  } else {
+    if (!ipCache[req.ip]) ipCache[req.ip] = 0;
+    ipCache[req.ip]++;
+
+    if (ipCache[req.ip] > 30) {
+      res.status(429).send("429 Too Many Requests");
+      return false;
+    }
+  }
+
+  return true;
+}
