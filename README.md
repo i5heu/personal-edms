befb8b97a8db783f34ba8c56eace14e84b536c94
i5heu <i5heu@i5h.eu>
2021-02-19 20:25:40 +0100

    Revert "Mvp (#1)"
    This reverts commit 781721d1f19abcca8cbd424d436cec0fec47fc59.

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
 # personal-edms
-
-A fast, small and easy maintenance EDMS for private life simplification.
-
-Supporting: 
-- Full Text Search
-- OCR
-- PDF Search
-- Simple API
-- Encrypted Auto Backup to S3
--- src/limitChecks.ts
+++ src/limitChecks.ts
@@ -4,11 +4,7 @@ let ipCache: number[] = [];
 let ipLoginCache: number[] = [];
 export function resetIpCache() {
   ipCache = [];
-  setTimeout(resetIpCache, 10000); // 10sec
-}
-export function resetLoginIpCache() {
-  ipLoginCache = [];
-  setTimeout(resetIpCache, 300000); // 5min
+  setTimeout(resetIpCache, 10000);
 }
 
 export function loginLeft(req: express.Request): number {
@@ -16,30 +12,24 @@ export function loginLeft(req: express.Request): number {
   return 5 - ipLoginCache[req.ip];
 }
 
-export function reqLeft(req: express.Request): number {
-  if (!ipCache[req.ip]) return 30;
-  return 30 - ipCache[req.ip];
-}
-
 export function reqCheck(
   req: express.Request,
   res: express.Response,
   login = false
 ): boolean {
-  if (!ipLoginCache[req.ip]) ipLoginCache[req.ip] = 0;
-  if (!ipCache[req.ip]) ipCache[req.ip] = 0;
-
-  ipCache[req.ip]++;
-
-  if (ipCache[req.ip] > 30) {
-    res.status(429).send("429 Too Many Requests");
-    return false;
-  }
-
   if (login) {
+    if (!ipLoginCache[req.ip]) ipLoginCache[req.ip] = 0;
     ipLoginCache[req.ip]++;
 
     if (ipLoginCache[req.ip] > 5) {
+      res.status(429).send("429 Too Many Requests");
+      return false;
+    }
+  } else {
+    if (!ipCache[req.ip]) ipCache[req.ip] = 0;
+    ipCache[req.ip]++;
+
+    if (ipCache[req.ip] > 30) {
       res.status(429).send("429 Too Many Requests");
       return false;
     }
